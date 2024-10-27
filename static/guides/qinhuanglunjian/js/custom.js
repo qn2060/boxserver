a521f39e8799f839805017bd70ce9dd0507c9271
qn2060 <qn2060@users.noreply.github.com>
2024-10-27 08:42:11 +0800

    Add files via upload

---
+++ static/guides/qinhuanglunjian/js/custom.js
@@ -0,0 +1,11 @@
+var indextitle = "秦皇爆率查询系统";
+var gmqqhao = "314888795";
+
+var gmqqqun = "314888795";
+
+var qqunlink = "https://qm.qq.com/q/RvJOJkg5iO";
+var version_list = [{
+name: "秦皇论剑",
+data: "qinhuanglunjian"
+,
+gllink: ""}];
